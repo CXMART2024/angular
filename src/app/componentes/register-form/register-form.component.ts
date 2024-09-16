fc9637afc7107efefa4227a40147248c96938c84
Luis <luis.15598@hotmail.com>
2024-09-16 13:49:28 -0500

    version 9.6

--- src/app/componentes/register-form/register-form.component.ts
+++ src/app/componentes/register-form/register-form.component.ts
@@ -251,13 +251,7 @@ export class RegisterFormComponent {
   formatDate(inputDate: string | null | undefined): string {
     if (!inputDate) return '';
   
-    const date = new Date(inputDate);
-  
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const year = date.getUTCFullYear();
-    
-    return `${day}/${month}/${year}`;
+    return moment(inputDate).format('DD/MM/YYYY');
   }
   
 
