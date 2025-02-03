63bc96e24bbf407ec4b462cf665e8607fb3abc29
frlynjoy <frlyn.joy@gmail.com>
2025-02-03 15:33:12 -0500

    update report

--- src/app/componentes/reporte-academico/reporte-academico.component.ts
+++ src/app/componentes/reporte-academico/reporte-academico.component.ts
@@ -1,8 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SolicitudService } from '../../servicios/solicitud/solicitud.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../servicios/auth/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { CursoService } from '../../servicios/curso/curso.service';
+import { Curso } from '../../modelos/curso';
+import { CicloService } from '../../servicios/ciclo/ciclo.service';
+import { Ciclo } from '../../modelos/ciclo';
 declare var bootstrap: any;
 
 @Component({
@@ -13,6 +17,12 @@ declare var bootstrap: any;
 export class ReporteAcademicoComponent implements OnInit {
 
   solicitud: any;
+  selectedCicloId: number = 0;
+  listCurso: Array<Curso> = [];
+  promedio: number = 0;
+  cicloData: Ciclo | null = null;
+  listCiclo: Array<Ciclo> = [];
+
   formUpdateLogin = this.formBuilder.group({
     dni: ['', [Validators.required]],
     antiguaClave: ['', [Validators.required]],
@@ -20,7 +30,7 @@ export class ReporteAcademicoComponent implements OnInit {
   });
 
   constructor(private solicitudService: SolicitudService, private authService: AuthService,
-    private formBuilder: FormBuilder, private toastr: ToastrService) {
+    private formBuilder: FormBuilder, private toastr: ToastrService, private cursoAcademico: CursoService, private cicloAcademico: CicloService, private cdr: ChangeDetectorRef) {
 
   }
   ngOnInit(): void {
@@ -53,6 +63,50 @@ export class ReporteAcademicoComponent implements OnInit {
         console.error('Error actualizando clave', error);
         this.formUpdateLogin.reset();
         this.toastr.error(`Error actualizando clave. Por favor, refresca la página y vuelve a intentarlo.`);
+      }
+    });
+  }
+
+  obtenerCursosDeCiclo(ciclo: any) {
+    this.selectedCicloId = ciclo.target.value;
+
+    this.cursoAcademico.getCursoByCiclo(this.selectedCicloId).subscribe({
+      next: (listCurso: Array<Curso>) => {
+        this.listCurso = listCurso;
+
+        this.promedio = this.sumarPromedio();
+
+
+
+      },
+      error: (error) => {
+        console.error('Error obteniendo cursos:', error);
+      },
+    });
+
+    this.getDataCiclo();
+  }
+
+  sumarPromedio(): number {
+    if (this.listCurso.length === 0) return 0;
+    const totalNotas = this.listCurso.reduce((total, curso) => total + (curso.nota || 0), 0);
+    const promedio = totalNotas / this.listCurso.length;
+    //console.log(`Total Notas: ${totalNotas}, Promedio: ${promedio}`);
+    return parseFloat(promedio.toFixed(2));
+  }
+
+  getDataCiclo(): void {
+    this.cicloAcademico.getCiclo(this.selectedCicloId).subscribe({
+      next: (cicloDetails: Ciclo[]) => {
+        if (cicloDetails.length > 0) {
+          this.cicloData = cicloDetails[0];
+        } else {
+          this.cicloData = null;
+        }
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error obteniendo ciclo:', error);
       }
     });
   }
